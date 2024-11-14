8e30a5348ff7fddef7846a6959f8075bc44c09da
ttb06 <112877189+ttb06@users.noreply.github.com>
2024-11-14 15:27:06 +0700

    Create autograd.py

---
+++ autograd.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+import torch
+
+weights=torch.ones(4,requires_grad=True)
+
+for epoch in range(10):
+    model_output = (weights*3).sum()
+
+    model_output.backward()
+
+    print(weights.grad)
+
+    weights.grad.zero_()
